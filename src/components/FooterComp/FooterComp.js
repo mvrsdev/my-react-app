2f81dcea2d8395a355e747d6edc108a0e6940605
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-06-04 07:00:33 -0300

    Create footer component

---
+++ src/components/FooterComp/FooterComp.js
@@ -0,0 +1,18 @@
+import './FooterComp.css';
+export const FooterComp = () => {
+  return (
+    <footer>
+      <div id="footer-left">
+        <a href="index.com">Sobre</a>
+        <a href="index.com">Publicidade</a>
+        <a href="index.com">Negócios</a>
+        <a href="index.com">Como funciona a Pesquisa</a>
+      </div>
+      <div id="footer-rigth">
+        <a href="index.com">Privacidade</a>
+        <a href="index.com">Termos</a>
+        <a href="index.com">Configurações</a>
+      </div>
+    </footer>
+  );
+};
