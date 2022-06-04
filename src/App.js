b2d8ea2bec0345d03edc4814fdbe44e7906d0811
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-06-04 07:06:48 -0300

    Refactor app js and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { HeaderComp } from './components/HeaderComp/HeaderComp';
+import { MainComp } from './components/MainComp/MainComp';
+import { FooterComp } from './components/FooterComp/FooterComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComp />
+      <MainComp />
+      <FooterComp />
     </div>
   );
 }
